c6162c524f55e09c18dfb732e445aafd2a04a5d8
Jose Pedro <pedroosd28@gmail.com>
2020-08-14 14:59:05 -0300

    Deploying to Heroku

--- gestaoclientes/settings.py
+++ gestaoclientes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://gestao-clientes-br.herokuapp.com/admin']
+ALLOWED_HOSTS = ['gestao-clientes-br.herokuapp.com']
 
 
 # Application definition
